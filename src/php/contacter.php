db632081f8050e5371a93272529634704ff5e5b3
Emile-G <egardent.pro@gmail.com>
2020-06-04 18:02:48 +0200

    Changes to be committed: 	modified:   article.html 	modified:   articles/docker.html 	modified:   articles/laravel.html 	modified:   articles/symfony.html 	modified:   cv.html 	modified:   index.html 	new file:   src/php/contacter.php 	modified:   veilles/juridiques/index.html 	modified:   veilles/technologiques/index.html

---
+++ src/php/contacter.php
@@ -0,0 +1,19 @@
+<?php 
+    $Nom     = $_GET['Nom'];
+    $Prénom  = $_GET['Prénom'];
+    $Email   = $_GET['Email'];
+    $Message = $_GET['Message'];
+
+    $Message = 'Expéditeur : ' . $Email . "\r\n" .
+               'Nom : ' . $Nom . "\r\n" .
+               'Prénom : ' . $Prénom . "\r\n" .
+               'Message : ' . $Message;
+
+    $To      = 'egardent.pro@gmail.com';
+    $Objet   = $_GET['Objet'];
+    $Headers = 'From: site@egardent.fr' . "\r\n" .
+               'Reply-To: site@egardent.fr' . "\r\n" ;
+    
+    mail($To, $Objet, $Message, $Headers);
+    header("Location: https://www.egardent.fr/"); 
+    exit();
